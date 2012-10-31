aa6694381f0bc2dc9052769267dc35b6e2c1bba1
Albert Latacz <voxavion@gmail.com>
2012-10-31 20:29:19 +0000

    first commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject function-this-up-clj "1.0.0-SNAPSHOT"
+  :description "Project for a brown-bag at Sky to convert imperative code to functional in clojure (https://github.com/raymanoz/function-this-up)"
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  )
